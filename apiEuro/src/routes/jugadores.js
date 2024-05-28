76d69d422f7b1ff5075121278a5c09a73b7b4639
victor3rey <99491227+victor3rey@users.noreply.github.com>
2024-05-28 20:47:06 +0200

    Update jugadores.js

--- apiEuro/src/routes/jugadores.js
+++ apiEuro/src/routes/jugadores.js
@@ -7,33 +7,29 @@ router.get('/', (req, res) => {
     SELECT j.ficha, j.Nombre, j.posicion, j.dorsal, s.NombreEquipo, j.imagen AS imagenJugador, s.imagen AS imagenSeleccion
     FROM jugadores j
     INNER JOIN selecciones s ON j.idEquipo = s.id
-`
-;
+`;
 
-
-db.query(query, (err, results) => {
-    if (err) {
-        console.error('Error executing query:', err);
-        res.status(500).json({ error: 'Internal Server Error' });
-        return;
-    }
-
-    
-
-    // Convertir imagen binaria a base64
-    const playersWithBase64Images = results.map(player => {
-        if (player.imagenJugador) {
-            player.imagenJugador = Buffer.from(player.imagenJugador).toString('base64');
-          
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
         }
-        if (player.imagenSeleccion) {
-            player.imagenSeleccion = Buffer.from(player.imagenSeleccion).toString('base64');
-          }
-        return player;
-    });
 
-    res.json(playersWithBase64Images);
-});
+        // Convertir imagen binaria a base64
+        const playersWithBase64Images = results.map(player => {
+            if (player.imagenJugador) {
+                player.imagenJugador = Buffer.from(player.imagenJugador).toString('base64');
+            }
+            if (player.imagenSeleccion) {
+                player.imagenSeleccion = Buffer.from(player.imagenSeleccion).toString('base64');
+            }
+            return player;
+        });
+
+        res.json(playersWithBase64Images);
+    });
 });
 
 module.exports = router;
+
