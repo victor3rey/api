021da3aca2cfc321c32df918f532238a3cabbdbf
victor3rey <99491227+victor3rey@users.noreply.github.com>
2024-05-28 19:49:58 +0200

    Update index.js

--- apiEuro/index.js
+++ apiEuro/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2');
@@ -16,6 +17,23 @@ app.use(cors({
 
 app.use(bodyParser.json());
 
+// Configurar la conexión a la base de datos
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+connection.connect(err => {
+    if (err) {
+        console.error('Error conectando a la base de datos:', err.stack);
+        return;
+    }
+    console.log('Conectado a la base de datos con ID:', connection.threadId);
+});
+
 // Importar y usar las rutas
 const jugadoresRouter = require('./src/routes/jugadores');
 app.use('/jugadores', jugadoresRouter);
@@ -24,5 +42,4 @@ app.use('/selecciones', seleccionesRouter);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
 
