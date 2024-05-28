a2ca5ac8c2c53fb82f01353b2005028c83189a81
victor3rey <99491227+victor3rey@users.noreply.github.com>
2024-05-28 20:01:18 +0200

    Create index.js

---
+++ apiEuro/src/index.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+const cors = require('cors');
+const seleccionesRouter = require('./src/routes/selecciones');
+
+const app = express();
+const PORT = process.env.PORT || 3005;
+
+// Configurar CORS
+app.use(cors({
+    origin: 'http://localhost:8100', // El origen de tu aplicación Ionic
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.use(bodyParser.json());
+
+// Configurar la conexión a la base de datos
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+connection.connect(err => {
+    if (err) {
+        console.error('Error conectando a la base de datos:', err.stack);
+        return;
+    }
+    console.log('Conectado a la base de datos con ID:', connection.threadId);
+});
+
+// Importar y usar las rutas
+const jugadoresRouter = require('./src/routes/jugadores');
+app.use('/jugadores', jugadoresRouter);
+app.use('/selecciones', seleccionesRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
